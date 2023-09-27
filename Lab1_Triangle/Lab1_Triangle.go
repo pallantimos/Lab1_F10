68b1354edcee6125e96573e2496d81ad10ce4dc3
pallantimos <adondokov@Bk.ru>
2023-09-27 22:50:55 +0700

    Add check triangle for isosceles Added catching error when converting type string to float

--- Lab1_Triangle/Lab1_Triangle.go
+++ Lab1_Triangle/Lab1_Triangle.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func CalculateKindTriangle(lengthA string, lengthB string, lengthC string) (kindTriangle string, coordinate [3][2]int) {
+	floatLengthA, err := strconv.ParseFloat(lengthA, 64)
+	floatLengthB, err := strconv.ParseFloat(lengthB, 64)
+	floatLengthC, err := strconv.ParseFloat(lengthC, 64)
+
+	if floatLengthA == floatLengthB && floatLengthB == floatLengthC {
+		return "равнобедренный", coordinate
+	}
+
+	if err != nil {
+		fmt.Println("Ошибка при конвертации", err)
+		coordinate := [3][2]int{
+			{-2, -2},
+			{-2, -2},
+			{-2, -2},
+		}
+		return "", coordinate
+	}
+	return kindTriangle, coordinate
+}

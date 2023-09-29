c6face3bbc0bb717a07c5b39a29a4921c3f55936
pallantimos <adondokov@Bk.ru>
2023-09-29 18:14:03 +0700

    Add all needed checks

--- Lab1_Triangle/Lab1_Triangle.go
+++ Lab1_Triangle/Lab1_Triangle.go
@@ -10,8 +10,20 @@ func CalculateKindTriangle(lengthA string, lengthB string, lengthC string) (kind
 	floatLengthB, err := strconv.ParseFloat(lengthB, 64)
 	floatLengthC, err := strconv.ParseFloat(lengthC, 64)
 
+	if floatLengthA+floatLengthB > floatLengthC || floatLengthA+floatLengthC > floatLengthB || floatLengthB+floatLengthC > floatLengthA {
+		return "не треугольник", coordinate
+	}
 	if floatLengthA == floatLengthB && floatLengthB == floatLengthC {
+		return "равносторонний", coordinate
+	} else if floatLengthA == floatLengthB || floatLengthB == floatLengthC || floatLengthC == floatLengthA {
+		coordinate := [3][2]int{
+			{0, 0},
+			{50, 100},
+			{100, 0},
+		}
 		return "равнобедренный", coordinate
+	} else {
+		return "разносторонний", coordinate
 	}
 
 	if err != nil {
